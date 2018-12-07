c522bd7b217ca84a2e4c2f7e4d48217b069ab511
jstudenski <jstudenski@gmail.com>
2018-12-07 17:50:57 -0600

    null

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ const items = [
   {id: 2, group: 2, title: 'item 2', start_time: moment().add(-0.5, 'hour'), end_time: moment().add(0.5, 'hour')},
   {id: 3, group: 1, title: 'item 3', start_time: moment().add(2, 'hour'), end_time: moment().add(3, 'hour')}
 ]
-
 class App extends Component {
   render() {
     return (
